bbbaa25df0883b9109b3f510bb6d5114b6fe8d78
annaruban <anna14196@ukr.net>
2024-11-27 21:41:23 +0100

    i added some wisdom

---
+++ 160924_ptm/one_more_annaruban.py
@@ -0,0 +1,2 @@
+print("Always remember: ‘Hello, World!’ is where every programmer starts, but no one stops there.")
+print("Code is like humor. When you have to explain it, it’s probably not good. ")
